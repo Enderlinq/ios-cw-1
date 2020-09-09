0b126851f8dac7570e192acb233a4ff78cb8dd71
Enderlinq <Enderlinq@users.noreply.github.com>
2020-09-09 18:56:41 +0300

    CW-1-1

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+let name = "Wahj Bader Al-Duwaisan"
+var height = 167.0
+var age = 16
+var hobby = "writing"
+var hobby2 = "cooking"
+print("My name is \(name)\nI'm \(age)\nI enjoy \(hobby) and \(hobby2)")
