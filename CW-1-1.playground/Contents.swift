c77575b1aaf57b5cf2465878baf7d449a6ecf832
Enderlinq <Enderlinq@users.noreply.github.com>
2020-09-09 20:29:39 +0300

    CW-1-2

--- CW-1-1.playground/Contents.swift
+++ CW-1-1.playground/Contents.swift
@@ -1,7 +1,34 @@
 
-let name = "Wahj Bader Al-Duwaisan"
-var height = 167.0
+// CLASSWORK ONE
+let name = "Wahj"
+var height = 1.67
 var age = 16
-var hobby = "writing"
-var hobby2 = "cooking"
+var hobby = "Writing"
+var hobby2 = "Cooking"
 print("My name is \(name)\nI'm \(age)\nI enjoy \(hobby) and \(hobby2)")
+
+// CLASSWORK TWO
+let friendName = "Fiona"
+var friendHeight = 1.64
+var friendAge = 23
+var friendHobby = "Reading"
+var friendHobby2 = "Socializing"
+
+print("\nMy friend's name is \(friendName)\nand she's \(friendAge)\nand she enjoys \(friendHobby) and \(friendHobby2)")
+
+var ageDifference = friendAge - age
+print ("\nOur age difference is: \(ageDifference)")
+
+var hobbyDifferences = (hobby == friendHobby) || (hobby == friendHobby2) || (hobby2 == friendHobby) || (hobby2 == friendHobby2)
+if (hobbyDifferences == true)
+  {
+    print("We have something in common.")
+                                            }
+
+else
+ {
+      print("We have nothing in common.")
+                                            }
+
+var heightDifference = height - friendHeight
+print ("Our height difference is: \(heightDifference)")
